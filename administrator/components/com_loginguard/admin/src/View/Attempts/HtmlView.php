9b3296a9cf28e463655b2a193bef7a9a3b37f923
Hazim <azizan.hazim@mda.gov.my>
2026-05-13 14:54:15 +0800

    Issue #5: add attempts view

---
+++ administrator/components/com_loginguard/admin/src/View/Attempts/HtmlView.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Joomla\Component\LoginGuard\Administrator\View\Attempts;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+
+class HtmlView extends BaseHtmlView
+{
+    public function display($tpl = null)
+    {
+        parent::display($tpl);
+    }
+}
